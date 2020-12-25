4b8ab0f6a624c8e26a3fd41f743ba567068b2541
Giant Robot <yetanothergiantrobot007@gmail.com>
2020-12-24 21:22:04 -0500

    updated poller to run continuously

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM python:3
-RUN mkdir app/
+RUN mkdir app
 ADD app app/
 COPY requirements.txt /tmp
 COPY main.py /
-RUN pip install -r /tmp/requirements.txt
-CMD ["python", "/main.py"]
+RUN chmod 0744 /main.py
+RUN pip3.9 install -r /tmp/requirements.txt
+CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -2,26 +2,39 @@
 # Main entry point
 from app.observer import ETCCWatcher
 from app.common.misc import future_dates
+from time import sleep
 import os
 import requests
 import json
 
 GW_SERVER = os.environ['API_GW']
 GW_PORT = os.environ['API_GW_PORT']
-URL = f'http://{GW_SERVER}:{GW_PORT}/'
-UPDATE_SLOT_URL = f'{URL}update_slot'
+BREAK_TIME = os.environ.get('SLEEP_TIME', 2)
+DAYS_TO_WATCH = os.environ.get('DAYS', 7)
+#
+URL = f'http://{GW_SERVER}:{GW_PORT}'
+UPDATE_SLOT_URL = f'{URL}/update_slot'
 HEADERS = {'Content-type': 'application/json'}
 
 
+def retrieve_date_info(watcher_obj: ETCCWatcher, date:str) -> dict:
+    for _, month_, date_, slot_name, slot_free_count in watcher_obj.retrieve_for_date(date):
+        yield {"month": month_, "date": date_, "slot_name": slot_name, "slot_free_count": slot_free_count}
+
+
+def update_db(data: dict) -> None:
+    requests.post(UPDATE_SLOT_URL, data=json.dumps(data), headers=HEADERS)
+
+
 def main():
-    # GW_SERVER = os.environ['GW_ADDRESS']
-    # GW_PORT = os.environ['GW_PORT']
     etcc_watcher = ETCCWatcher()
-    for date in future_dates(14):
-        print(f"Posting database for {date}!")
-        for _, month_, date_, slot_name, slot_free_count in etcc_watcher.retrieve_for_date(date):
-            data = {"month": month_, "date": date_, "slot_name": slot_name, "slot_free_count": slot_free_count}
-            requests.post(UPDATE_SLOT_URL, data=json.dumps(data), headers=HEADERS)
+    print(f"Starting Tracker - days to watch {DAYS_TO_WATCH} , wait time {BREAK_TIME}s")
+    while True:
+        for date in future_dates(DAYS_TO_WATCH):
+            for data in retrieve_date_info(etcc_watcher, date):
+                update_db(data)
+            print(f"Posting database for {date}!")
+        sleep(BREAK_TIME)
 
 
 if __name__ == "__main__":
